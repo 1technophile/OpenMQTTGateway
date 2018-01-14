b8109f86f0a541621b9a594908e6f2ea06c7fec7
Chris Broekema <broekema@astron.nl>
2018-01-14 14:30:41 +0100

    re-add TSL2561 code that was accidentally removed

--- OpenMQTTGateway.ino
+++ OpenMQTTGateway.ino
@@ -240,6 +240,9 @@ void setup()
   #ifdef ZsensorBH1750
     setupZsensorBH1750();
   #endif
+  #ifdef ZsensorTSL2561
+    setupZsensorTSL2561();
+  #endif
   #ifdef ZgatewayIR
     setupIR();
   #endif
@@ -371,6 +374,9 @@ void loop()
     #endif
     #ifdef ZsensorBH1750
       MeasureLightIntensity(); //Addon to measure Light Intensity with a BH1750
+    #endif
+    #ifdef ZsensorTSL2561
+      MeasureLightIntensityTSL2561();
     #endif
     #ifdef ZsensorDHT
       MeasureTempAndHum(); //Addon to measure the temperature with a DHT
