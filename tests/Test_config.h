b25eacdcbbc24b4556f54c3c620f4b9904f5b378
Florian <1technophile@users.noreply.github.com>
2018-04-11 19:39:13 +0200

    config reorganization

--- tests/Test_config.h
+++ tests/Test_config.h
@@ -83,82 +83,46 @@ const byte subnet[] = { 255, 255, 255, 0 }; //ip adress
 //Addons and module management, comment the Z line and the config file if you don't use
 #ifdef ESP8266 // for nodemcu, weemos and esp8266
   #define ZgatewayRF
-  #include "config_RF.h"
   #define ZgatewaySRFB
-  #include "config_SRFB.h"
   #define ZgatewayRF2
   #define ZgatewayIR
-  #include "config_IR.h"
   #define ZgatewayBT
-  #include "config_BT.h"
   #define ZactuatorONOFF
-  #include "config_ONOFF.h"
   #define ZsensorINA226
-  #include "config_INA226.h"
   #define ZsensorHCSR501
-  #include "config_HCSR501.h"
   #define ZsensorADC
-  #include "config_ADC.h"
   #define ZsensorBH1750
-  #include "config_BH1750.h"
   #define ZsensorBME280
-  #include "config_TSL2561.h"
   #define ZsensorTSL2561
-  #include "config_BME280.h"
   #define ZsensorDHT // If you uncomment this you can't use I2C due to the fact that I2C use also D1
-  #include "config_DHT.h"
   #define ZgatewayRFM69 // If you uncomment this you can't use RF and BT due to the fact that RF use also D8 and BT use also D6/D7
-  #include "config_RFM69.h"
 #elif ESP32
   #define ZgatewayRF
-  #include "config_RF.h"
   #define ZgatewayRF2
   //#define ZgatewayIR
-  //#include "config_IR.h"
   #define ZgatewayBT
-  #include "config_BT.h"
   #define ZactuatorONOFF
-  #include "config_ONOFF.h"
   //#define ZsensorINA226
-  //#include "config_INA226.h"
   //#define ZsensorHCSR501
-  //#include "config_HCSR501.h"
   //#define ZsensorADC
-  //#include "config_ADC.h"
   //#define ZsensorBH1750
-  //#include "config_BH1750.h"
   //#define ZsensorBME280
-  //#include "config_BME280.h"
   #define ZsensorDHT // If you uncomment this you can't use I2C due to the fact that I2C use also D1
-  #include "config_DHT.h"
   //#define ZgatewayRFM69 // If you uncomment this you can't use RF and BT due to the fact that RF use also D8 and BT use also D6/D7
-  //#include "config_RFM69.h"
 #else // for arduino mega + W5100
   #define ZgatewayRF
-  #include "config_RF.h"
   //#define ZgatewayRF2 // too big for UNO
   //#define ZgatewayRFM69 not tested
-  //#include "config_RFM69.h"
   //#define ZgatewayIR
-  //#include "config_IR.h"
   //#define ZgatewayBT
-  //#include "config_BT.h"
   //#define ZactuatorONOFF
-  //#include "config_ONOFF.h"
   //#define ZsensorINA226
-  //#include "config_INA226.h"
   //#define ZsensorDHT
-  //#include "config_DHT.h"
   //#define ZsensorBH1750
-  //#include "config_BH1750.h"
   //#define ZsensorBME280
-  //#include "config_BME280.h"
   //#define ZsensorHCSR501
-  //#include "config_HCSR501.h"
   //#define ZsensorADC
-  //#include "config_ADC.h"
   //#define ZgatewayRFM69 not tested
-  //#include "config_RFM69.h"  
 #endif
 /*----------------------------OTHER PARAMETERS-----------------------------*/
 #ifdef ZgatewaySRFB
