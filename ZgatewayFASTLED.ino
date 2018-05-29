1a3fc50e8f803676257788ca766f230a15e8f5c4
joen <joen@users.noreply.github.com>
2018-05-29 20:21:52 +0200

    num_leds fixed
    number of leds corrected for effects

--- ZgatewayFASTLED.ino
+++ ZgatewayFASTLED.ino
@@ -1,15 +1,15 @@
-/*  
-  OpenMQTTGateway - ESP8266 or Arduino program for home automation 
+/*
+  OpenMQTTGateway - ESP8266 or Arduino program for home automation
 
-   Act as a wifi or ethernet gateway between your 433mhz/infrared IR signal and a MQTT broker 
+   Act as a wifi or ethernet gateway between your 433mhz/infrared IR signal and a MQTT broker
    Send and receiving command by MQTT
- 
+
   This gateway enables to:
 
     Copyright: (c)Florian ROBERT
-  
+
     This file is part of OpenMQTTGateway.
-    
+
     OpenMQTTGateway is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -26,71 +26,112 @@
 #ifdef ZgatewayFASTLED
 
 #ifdef ESP8266
-  #include <FastLED.h>
+#include <FastLED.h>
 #else
-  #include <FastLED.h>
+#include <FastLED.h>
 #endif
 
 CRGB leds[FASTLED_NUM_LEDS];
 
-void setupFASTLED(){
-trc(F("FASTLED_DATA_PIN "));
-trc(String(FASTLED_DATA_PIN));
-trc(F("FASTLED_NUM_LEDS "));
-trc(String(FASTLED_NUM_LEDS));
-trc(F("ZgatewayIR setup done "));
+void setupFASTLED() {
+  trc(F("FASTLED_DATA_PIN "));
+  trc(String(FASTLED_DATA_PIN));
+  trc(F("FASTLED_NUM_LEDS "));
+  trc(String(FASTLED_NUM_LEDS));
+  trc(F("ZgatewayIR setup done "));
 
-  FastLED.addLeds<NEOPIXEL, FASTLED_DATA_PIN>(leds, FASTLED_NUM_LEDS); 
+  FastLED.addLeds<NEOPIXEL, FASTLED_DATA_PIN>(leds, FASTLED_NUM_LEDS);
 }
 
-boolean FASTLEDtoMQTT(){
-  return false;  
+boolean FASTLEDtoMQTT() {
+  return false;
 }
 
 void MQTTtoFASTLED(char * topicOri, char * datacallback) {
   trc(String(topicOri));
   long number = (long) strtol( &datacallback[1], NULL, 16);
-   if (String(topicOri) == "home/commands/MQTTtoFASTLED") {
-    leds[0] = number;
-    FastLED.show();
-  } else if (String(topicOri) == "home/commands/MQTTtoFASTLED/breath") {
-    leds[0] = number;
-    for(int j = 8200; j > 0; j--) {
-      float breath = (exp(sin(j/2000.0*PI)) - 0.36787944)*108.0;
-      FastLED.setBrightness(breath);
-      FastLED.show();
-      delay(1);
+  if (String(topicOri) == "home/commands/MQTTtoFASTLED") {
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = number;
     }
-    leds[0] = number;
     FastLED.show();
+  } else if (String(topicOri) == "home/commands/MQTTtoFASTLED/scan") {
+    for (int j = 10; j > 0; j--) {
+      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+        leds[i] = number;
+        leds[i - 1] = CRGB::Black;
+        FastLED.show();
+        delay(100);
+      }
+      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+        int k = FASTLED_NUM_LEDS - i - 1;
+        leds[k] = number;
+        leds[k + 1] = CRGB::Black;
+        FastLED.show();
+        delay(100);
+      }
+    }
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = number;
+    }
+    FastLED.show();
+//  } else if (String(topicOri) == "home/commands/MQTTtoFASTLED/breath") {
+//    for (int j = 8200; j > 0; j--) {
+//      float breath = (exp(sin(j / 2000.0 * PI)) - 0.36787944) * 108.0;
+//      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+//        FastLED.setBrightness(breath);
+//        leds[i] = number;
+//      }
+//      FastLED.show();
+//      delay(1);
+//    }
+//    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+//      leds[i] = number;
+//    }
+//    FastLED.show();
   } else if (String(topicOri) == "home/commands/MQTTtoFASTLED/alarm") {
-    for(int j = 100; j > 0; j--) {
-      leds[0] = number;
+    for (int j = 100; j > 0; j--) {
+      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+        leds[i] = number;
+      }
       FastLED.show();
       delay(200);
-      leds[0] = CRGB::Black;
+      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+        leds[i] = CRGB::Black;
+      }
       FastLED.show();
       delay(200);
     }
-    leds[0] = number;
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = number;
+    }
     FastLED.show();
   } else if (String(topicOri) == "home/commands/MQTTtoFASTLED/rainbow") {
-    for(int j = 0; j < 255; j++) {                     
-      int ihue = j; //some microcontrollers use HSV from 0-255 vs the normal 0-360               
-      if (ihue > 255) {ihue = 0;}
-      for(int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+    for (int j = 0; j < 255; j++) {
+      int ihue = j; //some microcontrollers use HSV from 0-255 vs the normal 0-360
+      if (ihue > 255) {
+        ihue = 0;
+      }
+      for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
         leds[i] = CHSV(ihue, 255, 255);
       }
-      FastLED.show();    
+      FastLED.show();
       delay(100);
     }
-    leds[0] = number;
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = number;
+    }
     FastLED.show();
   } else {
-    leds[0] = CRGB::Green;
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = CRGB::Green;
+    }
+    FastLED.setBrightness(50);
     FastLED.show();
     delay(200);
-    leds[0] = CRGB::Black;
+    for (int i = 0 ; i < FASTLED_NUM_LEDS; i++ ) {
+      leds[i] = CRGB::Black;
+    }
     FastLED.show();
   }
 }
