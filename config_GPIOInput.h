0f118c2020d47765492f0732a77a3e2779eb7065
QuagmireMan <osugroup@gmail.com>
2018-08-23 15:07:29 -0400

    GPIOInput Third Time is a Charm! (#258)
    * Added 2 files via upload
    
    * Update OpenMQTTGateway.ino
    
    * Update Test_config.h

---
+++ config_GPIOInput.h
@@ -0,0 +1,38 @@
+/*  
+  OpenMQTTGateway  - ESP8266 or Arduino program for home automation 
+
+   Act as a wifi or ethernet gateway between your 433mhz/infrared IR signal  and a MQTT broker 
+   Send and receiving command by MQTT
+ 
+   This files enables to set your parameter for the GPIOInput sensor
+  
+    Copyright: (c)Florian ROBERT
+  
+    This file is part of OpenMQTTGateway.
+    
+    OpenMQTTGateway is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenMQTTGateway is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*----------------------------USER PARAMETERS-----------------------------*/
+/*-------------DEFINE YOUR MQTT PARAMETERS BELOW----------------*/
+#define subjectGPIOInputtoMQTT    Base_Topic Gateway_Name "/GPIOInputtoMQTT"
+#define GPIOInputDebounceDelay 60 //debounce time, increase if there are issues
+
+/*-------------------PIN DEFINITIONS----------------------*/
+#if defined(ESP8266) || defined(ESP32)
+  #define GPIOInput_PIN 5 //D5
+#else
+  #define GPIOInput_PIN 7
+#endif
+
