4baaa82ebe497fa3eecb54d906f1090849450d98
animavitis <rherok@gmail.com>
2018-11-04 20:59:53 +0100

    config update

--- config_RF.h
+++ config_RF.h
@@ -71,8 +71,8 @@ RF supported protocols
     #define RF_RECEIVER_PIN 0 // D3 on nodemcu
     #define RF_EMITTER_PIN 3 // RX on nodemcu
 #elif defined(ESP32)
-    #define RF_RECEIVER_PIN 13 // D13 on DOIT ESP32
-    #define RF_EMITTER_PIN 12 // D12 on DOIT ESP32
+    #define RF_RECEIVER_PIN 23 // D13 on DOIT ESP32
+    #define RF_EMITTER_PIN 13 // D12 on DOIT ESP32
 #else
     //IMPORTANT NOTE: On arduino UNO connect IR emitter pin to D9 , comment #define IR_USE_TIMER2 and uncomment #define IR_USE_TIMER1 on library <library>IRremote/boarddefs.h so as to free pin D3 for RF RECEIVER PIN
     //RF PIN definition
