a74eb769312a3e40cfcedc8515c0a150f27dbb30
animavitis <rherok@gmail.com>
2018-11-09 22:19:02 +0100

    fix ESP32 ble (#304)
    * remove redundant code for home presence
    
    * hopefully fix ESP32 BLE hangs
    
    * basic support for duplicate detection when JSON publishing is active

--- config_BT.h
+++ config_BT.h
@@ -35,7 +35,7 @@
 
 /*-------------------HOME ASSISTANT ROOM PRESENCE ----------------------*/
 // if not commented Home presence integration with HOME ASSISTANT is activated
-#define subjectHomePresence  Base_Topic "/salon" // will send Home Assistant room presence message to this topic (first part is same for all rooms, second is room name)
+#define subjectHomePresence Base_Topic "home_presence/" Gateway_Name // will send Home Assistant room presence message to this topic (first part is same for all rooms, second is room name)
 
 
 struct decompose
