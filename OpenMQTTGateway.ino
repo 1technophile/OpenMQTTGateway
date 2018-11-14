bbb0665b236332cf46a91b9642fc24ca3756b034
Florian <1technophile@users.noreply.github.com>
2018-11-14 18:56:49 +0100

    correct sonoff rf bridge issue not communicating with RF module
    #310

--- OpenMQTTGateway.ino
+++ OpenMQTTGateway.ino
@@ -244,9 +244,10 @@ void setup()
   Serial.begin(SERIAL_BAUD);
   
   #if defined(ESP8266) || defined(ESP32)
-    #ifdef ESP8266
-      Serial.end();
-      Serial.begin(SERIAL_BAUD, SERIAL_8N1, SERIAL_TX_ONLY);
+      #ifndef ZgatewaySRFB // if we are not in sonoff rf bridge case we apply the ESP8266 pin optimization
+        Serial.end();
+        Serial.begin(SERIAL_BAUD, SERIAL_8N1, SERIAL_TX_ONLY);// enable on ESP8266 to free some pin
+      #endif
     #endif
     #if defined(ESP8266) && !defined(ESPWifiManualSetup)
       setup_wifimanager();
