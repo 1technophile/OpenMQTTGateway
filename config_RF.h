3b17625b25048355447a92dfef9b49aff7337fa8
Florian <1technophile@users.noreply.github.com>
2019-02-21 08:04:01 +0100

    change default pin for rf receiver on ESP32
    13 seems to don't work

--- config_RF.h
+++ config_RF.h
@@ -71,11 +71,11 @@ RF supported protocols
     #define RF_RECEIVER_PIN 0 // D3 on nodemcu
     #define RF_EMITTER_PIN 3 // RX on nodemcu if it doesn't work with 3, try with 4 (D2)
 #elif ESP32
-    #define RF_RECEIVER_PIN 13 // D13 on DOIT ESP32
+    #define RF_RECEIVER_PIN 27 // D27 on DOIT ESP32
     #define RF_EMITTER_PIN 12 // D12 on DOIT ESP32
 #elif __AVR_ATmega2560__
     #define RF_RECEIVER_PIN 1  //1 = D3 on mega
-    #define RF_EMITTER_PIN 4 
+    #define RF_EMITTER_PIN 4
 #else
     //IMPORTANT NOTE: On arduino UNO connect IR emitter pin to D9 , comment #define IR_USE_TIMER2 and uncomment #define IR_USE_TIMER1 on library <library>IRremote/boarddefs.h so as to free pin D3 for RF RECEIVER PIN
     //RF PIN definition
