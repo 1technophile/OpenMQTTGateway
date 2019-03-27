2ff5d6a25220718374af12126d722318cd8e3932
Florian <1technophile@users.noreply.github.com>
2019-03-27 20:22:20 +0100

    Add pilight repeater function
    #369
    set
    #define repeatPilightwMQTT true
    into config_RF.h to activate it

--- config_RF.h
+++ config_RF.h
@@ -65,11 +65,12 @@ RF supported protocols
 #define subjectPilighttoMQTT  Base_Topic Gateway_Name "/PilighttoMQTT"
 #define subjectGTWPilighttoMQTT  Base_Topic Gateway_Name "/PilighttoMQTT"
 #define PilightRAW "RAW"
+#define repeatPilightwMQTT false // do we repeat a received signal by using mqtt with Pilight gateway
 
 /*-------------------PIN DEFINITIONS----------------------*/
 #ifdef ESP8266
-    #define RF_RECEIVER_PIN 0 // D3 on nodemcu
-    #define RF_EMITTER_PIN 3 // RX on nodemcu if it doesn't work with 3, try with 4 (D2)
+    #define RF_RECEIVER_PIN 0 // D3 on nodemcu // put 4 with rf bridge direct mod 
+    #define RF_EMITTER_PIN 3 // RX on nodemcu if it doesn't work with 3, try with 4 (D2) // put 5 with rf bridge direct mod
 #elif ESP32
     #define RF_RECEIVER_PIN 27 // D27 on DOIT ESP32
     #define RF_EMITTER_PIN 12 // D12 on DOIT ESP32
