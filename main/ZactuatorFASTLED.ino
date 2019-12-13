1technophile/OpenMQTTGateway

commit 4b1f09b06bb317cca0df74e6db3f93d8e5706195

Wifi managers improvements (#503)
* Add parameters to wifi manager: gateway name and base topic
Enable to restart the ESP and erase the parameters by MQTT

main/ZactuatorFASTLED.ino
@@ -127,14 +127,12 @@ void MQTTtoFASTLEDJSON(char *topicOri, JsonObject&  jsonData)
 {
   trc(F("MQTTtoFASTLEDJSON: "));
   currentLEDState = GENERAL;
-  String topic = topicOri;
-  
-  trc(topic);
+  trc(topicOri);
   //number = (long)strtol(&datacallback[1], NULL, 16);
 
   
   
-  if (topic == subjectMQTTtoFASTLEDsetled)
+  if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetled)) != NULL)
   {
       trc(F("JSON parsed"));
       int ledNr = jsonData["led"];
@@ -160,10 +158,9 @@ void MQTTtoFASTLED(char *topicOri, char *datacallback)
 {
   trc(F("MQTTtoFASTLED: "));
   currentLEDState = GENERAL;
-  String topic = topicOri;
   long number = 0;
-  trc(topic);
-  if (topic == subjectMQTTtoFASTLED)
+  trc(topicOri);
+  if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLED)) != NULL)
   {
     number = (long)strtol(&datacallback[1], NULL, 16);
     trc(number);
@@ -173,18 +170,18 @@ void MQTTtoFASTLED(char *topicOri, char *datacallback)
     }
     FastLED.show();
   }
-  else if (topic == subjectMQTTtoFASTLEDsetbrightness)
+  else if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetbrightness)) != NULL)
   {
     number = (long)strtol(&datacallback[1], NULL, 16);
     trc(number);
     FastLED.setBrightness(number);
     FastLED.show();
   }
-  else if (topic == subjectMQTTtoFASTLEDsetanimation)
+  else if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetanimation)) != NULL)
   {
     String payload = datacallback;
     trc(payload);
-    if (payload.equals("fire"))
+    if ( strstr(datacallback,"fire") != NULL)
     {
       currentLEDState = FIRE;
       gPal = HeatColors_p;

main/ZactuatorONOFF.ino
@@ -33,7 +33,7 @@
 #ifdef jsonReceiving
 void MQTTtoONOFF(char * topicOri, JsonObject& ONOFFdata){
   
-  if (strcmp(topicOri,subjectMQTTtoONOFF) == 0){
+  if (strstr(topicOri,catToMainTopic(subjectMQTTtoONOFF)) != NULL){
     trc(F("MQTTtoONOFF json data analysis"));
     int boolSWITCHTYPE = ONOFFdata["state"] | 99;
     int pin = ONOFFdata["pin"] | ACTUATOR_ONOFF_PIN;
@@ -55,7 +55,7 @@ void MQTTtoONOFF(char * topicOri, JsonObject& ONOFFdata){
 
 #ifdef simpleReceiving
 void MQTTtoONOFF(char * topicOri, char * datacallback) {
-  if ((strstr(topicOri,subjectMQTTtoONOFF) != NULL) ){
+  if ((strstr(topicOri,catToMainTopic(subjectMQTTtoONOFF)) != NULL) ){
 
     trc(F("MQTTtoONOFF"));
     int pin = strtol(datacallback, NULL, 10); // we will not be able to pass values > 4294967295
@@ -69,7 +69,8 @@ void MQTTtoONOFF(char * topicOri, char * datacallback) {
 
     digitalWrite(pin, ON);
     // we acknowledge the sending by publishing the value to an acknowledgement topic
-    pub(subjectGTWONOFFtoMQTT, ON);
+    char b = ON;
+    pub(subjectGTWONOFFtoMQTT, &b);
   } 
 }
 #endif

main/Zgateway2G.ino
@@ -107,7 +107,7 @@ bool _2GtoMQTT(){
     String data = datacallback;
     String topic = topicOri;
     
-    if (topic == subjectMQTTto2G) {
+    if (strstr(topicOri,catToMainTopic(subjectMQTTto2G)) != NULL) {
       trc(F("MQTTto2G data analysis"));
       // 2G DATA ANALYSIS
       String phone_number = "";
@@ -122,7 +122,7 @@ bool _2GtoMQTT(){
         if (A6l.sendSMS(phone_number,data) == A6_OK ) {
           trc(F("SMS OK"));
           // Acknowledgement to the GTW2G topic
-          pub(subjectGTW2GtoMQTT, datacallback);// we acknowledge the sending by publishing the value to an acknowledgement topic, for the moment even if it is a signal repetition we acknowledge also
+          pub(subjectGTW2GtoMQTT, "SMS OK");// we acknowledge the sending by publishing the value to an acknowledgement topic, for the moment even if it is a signal repetition we acknowledge also
         }else{
           trc(F("SMS KO"));
           // Acknowledgement to the GTW2G topic
@@ -138,7 +138,7 @@ bool _2GtoMQTT(){
 #ifdef jsonReceiving
   void MQTTto2G(char * topicOri, JsonObject& SMSdata) {
     
-   if (strcmp(topicOri,subjectMQTTto2G) == 0){
+   if (strstr(topicOri,catToMainTopic(subjectMQTTto2G)) != NULL){
       const char * sms = SMSdata["message"];
       const char * phone = SMSdata["phone"];
       trc(F("MQTTto2G json data analysis"));
@@ -148,7 +148,7 @@ bool _2GtoMQTT(){
         if (A6l.sendSMS(String(phone),String(sms)) == A6_OK ) {
           trc(F("SMS OK"));
           // Acknowledgement to the GTW2G topic
-          pub(subjectGTW2GtoMQTT, SMSdata);// we acknowledge the sending by publishing the value to an acknowledgement topic, for the moment even if it is a signal repetition we acknowledge also
+          pub(subjectGTW2GtoMQTT, "SMS OK");// we acknowledge the sending by publishing the value to an acknowledgement topic, for the moment even if it is a signal repetition we acknowledge also
         }else{
           trc(F("SMS KO"));
           pub(subjectGTW2GtoMQTT, "SMS KO");// we acknowledge the sending by publishing the value to an acknowledgement topic, for the moment even if it is a signal repetition we acknowledge also

main/ZgatewayBT.ino
@@ -530,7 +530,12 @@ void MiBandDiscovery(char * mac){
       while(true){
           trc(taskMessage);
           delay(BLEinterval);
-          BLEscan();
+          if (client.state() == 0) {
+            BLEscan();
+          }else{
+            trc("MQTT client disconnected no BLE scan");
+            delay(1000);
+          }
       }
     }
   
@@ -993,12 +998,13 @@ void haRoomPresence(JsonObject& HomePresence){
   HomePresence["distance"] = distance;
   trc(F("BLE DISTANCE :"));
   trc(distance);
-  pub(subjectHomePresence,HomePresence);
+  String topic = String(Base_Topic) + "home_presence/" + String(gateway_name);
+  pub_custom_topic((char *)topic.c_str(),HomePresence);
 }
 #endif
 
 void MQTTtoBT(char * topicOri, JsonObject& BTdata) { // json object decoding
- if (strcmp(topicOri,subjectMQTTtoBTset) == 0){
+ if (strstr(topicOri,catToMainTopic(subjectMQTTtoBTset)) != NULL){
     trc(F("MQTTtoBT json set"));
 
     // Black list & white list set

main/ZgatewayIR.ino
@@ -152,7 +152,7 @@ void IRtoMQTT(){
         storeValue(MQTTvalue);
         if (repeatIRwMQTT){
             trc(F("Pub. IR for rpt"));
-            pub(subjectMQTTtoIR,MQTTvalue);
+            pubMQTT(subjectMQTTtoIR,MQTTvalue);
         }
     }
     }
@@ -274,7 +274,7 @@ void IRtoMQTT(){
 #ifdef jsonReceiving
   void MQTTtoIR(char * topicOri, JsonObject& IRdata) {
 
-     if (strcmp(topicOri,subjectMQTTtoIR) == 0){ 
+     if (strstr(topicOri,catToMainTopic(subjectMQTTtoIR)) != NULL){ 
       trc(F("MQTTtoIR json"));
       uint64_t data = IRdata["value"];
       const char * raw = IRdata["raw"];

main/ZgatewayLORA.ino
@@ -87,7 +87,7 @@ void LORAtoMQTT(){
 
 #ifdef jsonReceiving
   void MQTTtoLORA(char * topicOri, JsonObject& LORAdata) { // json object decoding
-   if (strcmp(topicOri,subjectMQTTtoLORA) == 0){
+   if (strstr(topicOri,catToMainTopic(subjectMQTTtoLORA)) != NULL){
       trc(F("MQTTtoLORA json"));
       const char * message = LORAdata["message"];
       int txPower = LORAdata["txpower"]|LORA_TX_POWER;
@@ -120,7 +120,7 @@ void LORAtoMQTT(){
 #endif
 #ifdef simpleReceiving
   void MQTTtoLORA(char * topicOri, char * LORAdata) { // json object decoding
-   if (strcmp(topicOri,subjectMQTTtoLORA) == 0){
+   if (strstr(topicOri,catToMainTopic(subjectMQTTtoLORA)) != NULL){
         LoRa.beginPacket();
         LoRa.print(LORAdata);
         LoRa.endPacket();

main/ZgatewayPilight.ino
@@ -74,7 +74,7 @@ void MQTTtoPilight(char * topicOri, JsonObject& Pilightdata) {
 
   int result = 0;
   
-  if (strcmp(topicOri,subjectMQTTtoPilight) == 0){
+  if (strstr(topicOri,catToMainTopic(subjectMQTTtoPilight)) != NULL){
     trc(F("MQTTtoPilight json data analysis"));
     const char * message = Pilightdata["message"];
     const char * protocol = Pilightdata["protocol"];

main/ZgatewayRF.ino
@@ -132,7 +132,7 @@ void MQTTtoRF(char * topicOri, char * datacallback) {
     trc(valueBITS);
   }
   
-  if ((topic == subjectMQTTtoRF) && (valuePRT == 0) && (valuePLSL  == 0) && (valueBITS == 0)){
+  if ((strstr(topicOri,catToMainTopic(subjectMQTTtoRF)) != NULL) && (valuePRT == 0) && (valuePLSL  == 0) && (valueBITS == 0)){
     trc(F("MQTTtoRF dflt"));
     mySwitch.setProtocol(1,350);
     mySwitch.send(data, 24);
@@ -156,8 +156,7 @@ void MQTTtoRF(char * topicOri, char * datacallback) {
 
 #ifdef jsonReceiving
   void MQTTtoRF(char * topicOri, JsonObject& RFdata) { // json object decoding
-  
-   if (strcmp(topicOri,subjectMQTTtoRF) == 0){
+   if (strstr(topicOri,catToMainTopic(subjectMQTTtoRF)) != NULL){
       trc(F("MQTTtoRF json"));
       unsigned long data = RFdata["value"];
       if (data != 0) {

commit 7f16d00a55e9cdfeed27e2020d103daf6159a20f

Change topics concatenation method
Correct regression introduced by #503
Nevertheless this method needs to be optimized to avoid the use of string.

main/ZactuatorFASTLED.ino
@@ -132,7 +132,7 @@ void MQTTtoFASTLEDJSON(char *topicOri, JsonObject&  jsonData)
 
   
   
-  if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetled)) != NULL)
+  if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoFASTLEDsetled).c_str()) != NULL)
   {
       trc(F("JSON parsed"));
       int ledNr = jsonData["led"];
@@ -160,7 +160,7 @@ void MQTTtoFASTLED(char *topicOri, char *datacallback)
   currentLEDState = GENERAL;
   long number = 0;
   trc(topicOri);
-  if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLED)) != NULL)
+  if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoFASTLED).c_str()) != NULL)
   {
     number = (long)strtol(&datacallback[1], NULL, 16);
     trc(number);
@@ -170,14 +170,14 @@ void MQTTtoFASTLED(char *topicOri, char *datacallback)
     }
     FastLED.show();
   }
-  else if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetbrightness)) != NULL)
+  else if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoFASTLEDsetbrightness).c_str()) != NULL)
   {
     number = (long)strtol(&datacallback[1], NULL, 16);
     trc(number);
     FastLED.setBrightness(number);
     FastLED.show();
   }
-  else if (strstr(topicOri,catToMainTopic(subjectMQTTtoFASTLEDsetanimation)) != NULL)
+  else if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoFASTLEDsetanimation).c_str()) != NULL)
   {
     String payload = datacallback;
     trc(payload);

main/ZactuatorONOFF.ino
@@ -33,7 +33,7 @@
 #ifdef jsonReceiving
 void MQTTtoONOFF(char * topicOri, JsonObject& ONOFFdata){
   
-  if (strstr(topicOri,catToMainTopic(subjectMQTTtoONOFF)) != NULL){
+  if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoONOFF).c_str()) != NULL){
     trc(F("MQTTtoONOFF json data analysis"));
     int boolSWITCHTYPE = ONOFFdata["state"] | 99;
     int pin = ONOFFdata["pin"] | ACTUATOR_ONOFF_PIN;
@@ -55,7 +55,7 @@ void MQTTtoONOFF(char * topicOri, JsonObject& ONOFFdata){
 
 #ifdef simpleReceiving
 void MQTTtoONOFF(char * topicOri, char * datacallback) {
-  if ((strstr(topicOri,catToMainTopic(subjectMQTTtoONOFF)) != NULL) ){
+  if ((strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoONOFF).c_str()) != NULL) ){
 
     trc(F("MQTTtoONOFF"));
     int pin = strtol(datacallback, NULL, 10); // we will not be able to pass values > 4294967295

main/Zgateway2G.ino
@@ -107,7 +107,7 @@ bool _2GtoMQTT(){
     String data = datacallback;
     String topic = topicOri;
     
-    if (strstr(topicOri,catToMainTopic(subjectMQTTto2G)) != NULL) {
+    if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTto2G).c_str()) != NULL) {
       trc(F("MQTTto2G data analysis"));
       // 2G DATA ANALYSIS
       String phone_number = "";
@@ -138,7 +138,7 @@ bool _2GtoMQTT(){
 #ifdef jsonReceiving
   void MQTTto2G(char * topicOri, JsonObject& SMSdata) {
     
-   if (strstr(topicOri,catToMainTopic(subjectMQTTto2G)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTto2G).c_str()) != NULL){
       const char * sms = SMSdata["message"];
       const char * phone = SMSdata["phone"];
       trc(F("MQTTto2G json data analysis"));

main/ZgatewayBT.ino
@@ -1044,7 +1044,7 @@ void haRoomPresence(JsonObject& HomePresence){
 #endif
 
 void MQTTtoBT(char * topicOri, JsonObject& BTdata) { // json object decoding
- if (strstr(topicOri,catToMainTopic(subjectMQTTtoBTset)) != NULL){
+ if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoBTset).c_str()) != NULL){
     trc(F("MQTTtoBT json set"));
 
     // Black list & white list set

main/ZgatewayIR.ino
@@ -274,7 +274,7 @@ void IRtoMQTT(){
 #ifdef jsonReceiving
   void MQTTtoIR(char * topicOri, JsonObject& IRdata) {
 
-     if (strstr(topicOri,catToMainTopic(subjectMQTTtoIR)) != NULL){ 
+     if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoIR).c_str()) != NULL){ 
       trc(F("MQTTtoIR json"));
       uint64_t data = IRdata["value"];
       const char * raw = IRdata["raw"];

main/ZgatewayLORA.ino
@@ -87,7 +87,7 @@ void LORAtoMQTT(){
 
 #ifdef jsonReceiving
   void MQTTtoLORA(char * topicOri, JsonObject& LORAdata) { // json object decoding
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoLORA)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoLORA).c_str()) != NULL){
       trc(F("MQTTtoLORA json"));
       const char * message = LORAdata["message"];
       int txPower = LORAdata["txpower"]|LORA_TX_POWER;
@@ -120,7 +120,7 @@ void LORAtoMQTT(){
 #endif
 #ifdef simpleReceiving
   void MQTTtoLORA(char * topicOri, char * LORAdata) { // json object decoding
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoLORA)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoLORA).c_str()) != NULL){
         LoRa.beginPacket();
         LoRa.print(LORAdata);
         LoRa.endPacket();

main/ZgatewayPilight.ino
@@ -74,7 +74,7 @@ void MQTTtoPilight(char * topicOri, JsonObject& Pilightdata) {
 
   int result = 0;
   
-  if (strstr(topicOri,catToMainTopic(subjectMQTTtoPilight)) != NULL){
+  if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoPilight).c_str()) != NULL){
     trc(F("MQTTtoPilight json data analysis"));
     const char * message = Pilightdata["message"];
     const char * protocol = Pilightdata["protocol"];

main/ZgatewayRF.ino
@@ -132,7 +132,7 @@ void MQTTtoRF(char * topicOri, char * datacallback) {
     trc(valueBITS);
   }
   
-  if ((strstr(topicOri,catToMainTopic(subjectMQTTtoRF)) != NULL) && (valuePRT == 0) && (valuePLSL  == 0) && (valueBITS == 0)){
+  if ((strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoRF).c_str()) != NULL) && (valuePRT == 0) && (valuePLSL  == 0) && (valueBITS == 0)){
     trc(F("MQTTtoRF dflt"));
     mySwitch.setProtocol(1,350);
     mySwitch.send(data, 24);
@@ -156,7 +156,7 @@ void MQTTtoRF(char * topicOri, char * datacallback) {
 
 #ifdef jsonReceiving
   void MQTTtoRF(char * topicOri, JsonObject& RFdata) { // json object decoding
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoRF)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoRF).c_str()) != NULL){
       trc(F("MQTTtoRF json"));
       unsigned long data = RFdata["value"];
       if (data != 0) {

main/ZgatewayRF2.ino
@@ -215,7 +215,7 @@ void rf2Callback(unsigned int period, unsigned long address, unsigned long group
 #ifdef jsonReceiving
   void MQTTtoRF2(char * topicOri, JsonObject& RF2data) { // json object decoding
   
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoRF2)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoRF2).c_str()) != NULL){
       trc(F("MQTTtoRF2 json"));
       int boolSWITCHTYPE = RF2data["switchType"] | 99;
       if (boolSWITCHTYPE != 99) {

main/ZgatewayRFM69.ino
@@ -184,7 +184,7 @@ bool RFM69toMQTT(void) {
 #ifdef simpleReceiving
   void MQTTtoRFM69(char * topicOri, char * datacallback) {
 
-    if(strstr(topicOri,catToMainTopic(subjectMQTTtoRFM69)) != NULL){
+    if(strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoRFM69).c_str()) != NULL){
       trc(F("MQTTtoRFM69 data analysis"));
       char data[RF69_MAX_DATA_LEN+1];
       memcpy(data, (void *)datacallback, RF69_MAX_DATA_LEN);
@@ -215,7 +215,7 @@ bool RFM69toMQTT(void) {
 #ifdef jsonReceiving
   void MQTTtoRFM69(char * topicOri, JsonObject& RFM69data) {
   
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoRFM69)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoRFM69).c_str()) != NULL){
       const char * data = RFM69data["data"];
       trc(F("MQTTtoRFM69 json data analysis"));
       if(data){

main/ZgatewaySRFB.ino
@@ -280,7 +280,7 @@ bool _rfbToChar(byte * in, char * out) {
     // RF DATA ANALYSIS
     const char * raw = SRFBdata["raw"];
     int valueRPT =  SRFBdata["repeat"]|1;
-   if (strstr(topicOri,catToMainTopic(subjectMQTTtoSRFB)) != NULL){
+   if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoSRFB).c_str()) != NULL){
       trc(F("MQTTtoSRFB json"));
       if (raw){ // send raw in priority when defined in the json
         trc(F("MQTTtoSRFB raw ok"));

main/main.ino
@@ -428,9 +428,9 @@ void pubMQTT(String topic, unsigned long payload){
     client.publish((char *)topic.c_str(),val);
 }
 
-char * catToMainTopic(char * toAdd){
+String catToMainTopic(char * toAdd){
   String resultTopic = String(mqtt_topic) + String(toAdd);
-  return (char *)resultTopic.c_str();
+  return resultTopic;
 }
 
 void reconnect() {
@@ -452,7 +452,7 @@ void reconnect() {
       // publish version
       pub(version_Topic,OMG_VERSION,will_Retain);
       //Subscribing to topic
-      if (client.subscribe(catToMainTopic(subjectMQTTtoX))) {
+      if (client.subscribe((char *)catToMainTopic(subjectMQTTtoX).c_str())) {
         #ifdef ZgatewayRF
           client.subscribe(subjectMultiGTWRF); // subject on which other OMG will publish, this OMG will store these msg and by the way don't republish them if they have been already published
         #endif
@@ -1320,7 +1320,7 @@ digitalWrite(led_send, HIGH);
 }
 
 void MQTTtoSYS(char * topicOri, JsonObject& SYSdata) { // json object decoding
- if (strstr(topicOri,catToMainTopic(subjectMQTTtoSYSset)) != NULL){
+ if (strstr(topicOri,(char *)catToMainTopic(subjectMQTTtoSYSset).c_str()) != NULL){
     trc(F("MQTTtoSYS json set"));
     #if defined(ESP8266) || defined(ESP32)
       if (SYSdata.containsKey("cmd")){