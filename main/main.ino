f991b209e5be0eda28085c66ac9e1641177e5ece
Florian <1technophile@users.noreply.github.com>
2020-03-22 20:29:21 -0500

    Add version into SYStoMQTT gateway status
    #537

--- main/main.ino
+++ main/main.ino
@@ -1165,6 +1165,7 @@ void stateMeasures()
     JsonObject &SYSdata = jsonBuffer.createObject();
     unsigned long uptime = millis() / 1000;
     SYSdata["uptime"] = uptime;
+    SYSdata["version"] = OMG_VERSION;
     Log.trace(F("retriving value of system characteristics Uptime (s):%u" CR),uptime);
     #if defined(ESP8266) || defined(ESP32)
       uint32_t freeMem;
