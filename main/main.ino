2dba3ac344543cab2fa6a0f129d929dbdc69fa1b
Jonathan Wells <kcender@gmail.com>
2020-07-21 22:51:07 +0900

    lint changes

--- main/main.ino
+++ main/main.ino
@@ -134,8 +134,8 @@ unsigned long timer_sys_measures = 0;
 void callback(char* topic, byte* payload, unsigned int length);
 
 #ifdef ESPWifiManualSetup
-  char manual_wifi_ssid[] = wifi_ssid;
-  char manual_wifi_password[] = wifi_password;
+char manual_wifi_ssid[] = wifi_ssid;
+char manual_wifi_password[] = wifi_password;
 #endif
 
 char mqtt_user[parameters_size] = MQTT_USER_DEFAULT; // not compulsory only if your broker needs authentication
