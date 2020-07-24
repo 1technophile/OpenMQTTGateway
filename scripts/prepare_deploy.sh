b7a45915184c072f8685d4f848b703f4b2cef77f
Leon Kiefer <leon.k97@gmx.de>
2020-07-24 15:45:29 +0200

    updated prepare deploy script to exclude some envs (#707)

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -4,13 +4,20 @@ echo "renaming bin files with the board name"
 rename -v 's:/:-:g' .pio/build/*/*.bin
 mkdir toDeploy
 rename 's/.pio-build-//' .*.bin
+# don't publish binaries for *-all and *-test env
+rm *-all.bin *-test.bin
 (
   cd .pio/libdeps
   echo "replace space by _ in folder names"
   find . -type d -name "* *" | while read FNAME; do mv "$FNAME" "${FNAME// /_}"; done
   echo "zipping libraries per board"
-  for i in */; do zip -r "${i%/}-libraries.zip" "$i"; done
-  ls -la
+  for i in */
+  do
+   zip -r "${i%/}-libraries.zip" "$i"
+  done
+  # don't publish libraries for *-test env
+  rm *-test.zip
+  ls -lA
   mv *.zip ../../toDeploy
 )
 
