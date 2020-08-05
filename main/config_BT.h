43606b0cb48d99dbd4879ecbc63fa701e3a24b9c
Jonathan Wells <kcender@gmail.com>
2020-08-05 22:07:49 +0900

    Minimize decompose, remove calls to extract_char, check data length before parsing
    LINT

--- main/config_BT.h
+++ main/config_BT.h
@@ -39,6 +39,10 @@ extern void MQTTtoBT(char* topicOri, JsonObject& RFdata);
 //#define HM-11 // uncomment this line if you use HM-11 and comment the line above
 //#define HM_BLUE_LED_STOP true //uncomment to stop the blue led light of HM1X
 #define BLEdelimiter         "4f4b2b444953413a" // OK+DISA:
+#define BLEEndOfDiscovery    "4f4b2b4449534345" // OK+DISCE
+#define BLEdelimiterLength   16
+#define CRLR                 "0d0a"
+#define CRLR_Length          4
 #define ServicedataMinLength 29
 
 #ifndef TimeBtw_Read
