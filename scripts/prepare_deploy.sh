046a251b0ca9f978e578e175f752989f73c5f1b6
Florian <1technophile@users.noreply.github.com>
2020-09-09 16:19:18 -0500

    [CI] Enable test env removing during the publish process

--- scripts/prepare_deploy.sh
+++ scripts/prepare_deploy.sh
@@ -4,8 +4,6 @@ echo "renaming bin files with the board name"
 rename -v 's:/:-:g' .pio/build/*/*.bin
 mkdir toDeploy
 rename 's/.pio-build-//' .*.bin
-# don't publish binaries for *-all and *-test env
-rm *-all.bin *-test.bin
 (
   cd .pio/libdeps
   echo "replace space by _ in folder names"
@@ -15,12 +13,11 @@ rm *-all.bin *-test.bin
   do
    zip -r "${i%/}-libraries.zip" "$i"
   done
-  # don't publish libraries for *-test env
-  rm *-test.zip
   ls -lA
   mv *.zip ../../toDeploy
 )
-
+# remove binaries for *-all*, *-test* env and only zip containing *-test*
+rm -f *-all*.bin *-test*.bin *-test*.zip
 echo "zipping code and licence"
 zip -r OpenMQTTGateway_sources.zip main LICENSE.txt
 mv *.zip toDeploy
