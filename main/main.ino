c2d2e72d62af9ff547d450ae38b9d46008988008
Charles R <hugo@digitalspirit.org>
2020-10-13 08:27:00 +0200

    feat: move getMacAddress to main
    We need it on the next commit.

--- main/main.ino
+++ main/main.ino
@@ -202,6 +202,21 @@ EthernetClient eClient;
 // client link to pubsub mqtt
 PubSubClient client(eClient);
 
+String getMacAddress() {
+  uint8_t baseMac[6];
+  char baseMacChr[13] = {0};
+#  if defined(ESP8266)
+  WiFi.macAddress(baseMac);
+  sprintf(baseMacChr, "%02X%02X%02X%02X%02X%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+#  elif defined(ESP32)
+  esp_read_mac(baseMac, ESP_MAC_WIFI_STA);
+  sprintf(baseMacChr, "%02X%02X%02X%02X%02X%02X", baseMac[0], baseMac[1], baseMac[2], baseMac[3], baseMac[4], baseMac[5]);
+#  else
+  sprintf(baseMacChr, "%02X%02X%02X%02X%02X%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+#  endif
+  return String(baseMacChr);
+}
+
 void revert_hex_data(const char* in, char* out, int l) {
   //reverting array 2 by 2 to get the data in good order
   int i = l - 2, j = 0;
