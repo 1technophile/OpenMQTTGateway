c3f2d6e9ad1fd9f175786c39a04ac3acf0a3f1b1
悠悠是梦 <radaig@163.com>
2021-02-24 21:43:22 +0800

    Update User_config.h

--- main/User_config.h
+++ main/User_config.h
@@ -115,12 +115,12 @@ const byte mac[] = {0xDE, 0xED, 0xBA, 0xFE, 0x54, 0x95}; //W5100 ethernet shield
 //MQTT Parameters definition
 #if defined(ESP8266) || defined(ESP32) || defined(__AVR_ATmega2560__) || defined(__AVR_ATmega1280__)
 #  define parameters_size      30
-#  define mqtt_topic_max_size  100
-#  define mqtt_max_packet_size 1024
+#  define mqtt_topic_max_size  200
+#  define mqtt_max_packet_size 2048
 #else
 #  define parameters_size      15
-#  define mqtt_topic_max_size  50
-#  define mqtt_max_packet_size 128
+#  define mqtt_topic_max_size  100
+#  define mqtt_max_packet_size 1024
 #endif
 
 // activate the use of TLS for secure connection to the MQTT broker
@@ -203,8 +203,8 @@ uint8_t wifiProtocol = 0; // default mode, automatic selection
 /*-------------DEFINE THE MODULES YOU WANT BELOW----------------*/
 //Addons and module management, uncomment the Z line corresponding to the module you want to use
 
-//#define ZgatewayRF     "RF"       //ESP8266, Arduino, ESP32
-//#define ZgatewayIR     "IR"       //ESP8266, Arduino,         Sonoff RF Bridge
+#define ZgatewayRF     "RF"       //ESP8266, Arduino, ESP32
+#define ZgatewayIR     "IR"       //ESP8266, Arduino,         Sonoff RF Bridge
 //#define ZgatewayLORA   "LORA"       //ESP8266, Arduino, ESP32
 //#define ZgatewayPilight "Pilight" //ESP8266, Arduino, ESP32
 //#define ZgatewayWeatherStation "WeatherStation" //ESP8266, Arduino, ESP32
