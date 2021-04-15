0cbac1309da55e12b115bfdb676bf384e6eb0b62
murfy76 <peter.paldan@gmail.com>
2021-04-15 14:27:12 +0200

    Add files via upload
    Added CCS811

---
+++ main/ZsensorCCS811.ino
@@ -0,0 +1,65 @@
+/*
+  Based on:
+  https://how2electronics.com/measure-co2-tvoc-using-ccs811-gas-sensor-arduino/
+  and ZsenzorBME280.ino
+*/
+#include "User_config.h"
+#include "Adafruit_CCS811.h"
+ 
+Adafruit_CCS811 ccs;
+ 
+void setupZsensorCCS811() {
+  Serial.begin(115200);
+  Serial.println("CCS811 test..."); 
+  if(!ccs.begin()){
+    Serial.println("Failed to start sensor! Please check your wiring.");
+    while(1);
+  }
+  // Wait for the sensor to be ready
+  while(!ccs.available());
+}
+ 
+void MeasureCO2AndTVOC() {
+  if(ccs.available()){
+    if(!ccs.readData()){
+      if (millis() > (timeccs811 + TimeBetweenReadingccs811)) {
+        timeccs811 = millis();
+        static float persisted_ccs_co2;
+        static float persisted_ccs_tvoc;
+    
+        float CcsCO2 = ccs.geteCO2();
+        float CcsTVOC = ccs.getTVOC();
+    
+        // Check if reads failed and exit early (to try again).
+        if (isnan(CcsCO2) || isnan(CcsTVOC)) {
+          Log.error(F("Failed to read from CCS811!" CR));
+        } else {
+          Log.trace(F("Creating CCS811 buffer" CR));
+          StaticJsonBuffer<JSON_MSG_BUFFER> jsonBuffer;
+          JsonObject& CCS811data = jsonBuffer.createObject();
+          // Generate CO2
+          if (CcsCO2 != persisted_ccs_co2 || ccs811_always) {
+            CCS811data.set("co2", (float)CcsCO2);
+          } else {
+            Log.trace(F("Same CO2 don't send it" CR));
+          }
+          // Generate TVOC
+          if (CcsTVOC != persisted_ccs_tvoc || ccs811_always) {
+            CCS811data.set("tvoc", (float)CcsTVOC);
+          } else {
+            Log.trace(F("Same TVOC don't send it" CR));
+          }
+          if (CCS811data.size() > 0)
+            pub(CCSTOPIC, CCS811data);
+        } 
+      persisted_ccs_co2 = CcsCO2;
+      persisted_ccs_tvoc = CcsTVOC;     
+      }
+    }
+    else{
+      Serial.println("ERROR!");
+      while(1);
+    }
+  }
+  delay(3000);
+}
