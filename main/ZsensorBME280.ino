48f441be1e59d70e36f9137633117e03667283dc
spacemanspiff2007 <10754716+spacemanspiff2007@users.noreply.github.com>
2021-05-29 17:51:54 +0200

    Update ZsensorBME280.ino
    fixes

--- main/ZsensorBME280.ino
+++ main/ZsensorBME280.ino
@@ -50,12 +50,12 @@ BME280 mySensor;
 
 void setupZsensorBME280() {
 
+# if defined(ESP8266) || defined(ESP32)
   // Allow custom pins on ESP Platforms
-  #if defined(ESP8266) || defined(ESP32)
-    Wire.setPins(BME280_PIN_SDA, BME280_PIN_SCL);
-  #endif
-
+  Wire.begin(BME280_PIN_SDA, BME280_PIN_SCL);
+# else
   Wire.begin();
+# endif
 
   mySensor.settings.commInterface = I2C_MODE;
   mySensor.settings.I2CAddress = BME280_i2c_addr;
