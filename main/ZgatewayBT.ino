5fabc0d60c8788255bdc582fa830ec9319581bf0
h2zero <powellperalta@gmail.com>
2022-01-26 19:25:59 -0700

    Update decoder version.

--- main/ZgatewayBT.ino
+++ main/ZgatewayBT.ino
@@ -824,8 +824,9 @@ void launchBTDiscovery() {
 #    endif
     BLEdevice* p = *it;
     Log.trace(F("Device mac %s" CR), p->macAdr);
-    if (p->sensorModel_id != TheengsDecoder::BLE_ID_NUM::UNKNOWN_MODEL &&
-        p->sensorModel_id < BLEconectable::id::MIN && !isDiscovered(p)) {
+    if (p->sensorModel_id > TheengsDecoder::BLE_ID_NUM::UNKNOWN_MODEL &&
+        p->sensorModel_id < TheengsDecoder::BLE_ID_NUM::BLE_ID_MAX &&
+        !isDiscovered(p)) {
       String macWOdots = String(p->macAdr);
       macWOdots.replace(":", "");
       Log.trace(F("Looking for Model_id: %d" CR), p->sensorModel_id);
