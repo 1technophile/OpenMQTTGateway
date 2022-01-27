ef41546c94e6b65b13bddfe1b53d732fee4e8ef8
Florian <1technophile@users.noreply.github.com>
2022-01-27 15:47:55 -0600

    ESP8266: Fix partial erase of connection settings (#1150)
    * Fix partial erase of connection settings
    
    #1149
    
    * Update WM version and add disconnect

--- main/main.ino
+++ main/main.ino
@@ -1047,7 +1047,8 @@ void eraseAndRestart() {
   Log.trace(F("Formatting requested, result: %d" CR), SPIFFS.format());
 
 #  if defined(ESP8266)
-  SPIFFS.format();
+  WiFi.disconnect(true);
+  wifiManager.resetSettings();
   delay(5000);
   ESP.reset();
 #  else
