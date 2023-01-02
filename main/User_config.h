526f1f4c52ea9bcc2c791df0667cae95da1e5269
Northern Man <northern.man1@gmail.com>
2023-01-01 21:28:35 -0500

    Lint issues

--- main/User_config.h
+++ main/User_config.h
@@ -566,7 +566,7 @@ CRGB leds[FASTLED_IND_NUM_LEDS];
 
 /*-----------PLACEHOLDERS FOR OLED/LCD DISPLAY--------------*/
 // The real definitions are in config_M5.h / config_SSD1306.h
-#define pubOled(...)   // only print if not in low power mode
+#define pubOled(...)        // display the published message onto the OLED display
 #define displayPrint(...)   // only print if not in low power mode
 #define lpDisplayPrint(...) // print in low power mode
 
