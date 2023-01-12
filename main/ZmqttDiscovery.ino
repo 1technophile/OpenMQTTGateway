2cfd1b4588159f975c8ba55e3e0f2ce6c3bf3c06
Florian <1technophile@users.noreply.github.com>
2023-01-12 16:23:55 -0600

    Fix internal temperature reading on ESP32
    The previous macro used SENS_SAR_MEAS_WAIT2_REG to identify if we have the reading capability was not working, so replacing it by a fresh one NO_INT_TEMP_READING, defined as a negation due to the fact that most of the ESP32 supports internal temperature reading.

--- main/ZmqttDiscovery.ino
+++ main/ZmqttDiscovery.ino
@@ -420,7 +420,7 @@ void pubMqttDiscovery() {
   );
 #    endif
 #  endif
-#  ifdef ESP32
+#  if defined(ESP32) && !defined(NO_INT_TEMP_READING)
   createDiscovery("sensor", //set Type
                   subjectSYStoMQTT, "SYS: Internal temperature", (char*)getUniqueId("tempc", "").c_str(), //set state_topic,name,uniqueId
                   "", "temperature", "{{ value_json.tempc }}", //set availability_topic,device_class,value_template,
