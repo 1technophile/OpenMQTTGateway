3a990b94a66aba7fa6c3530441a555f1d0c802ac
Merijn <19893132+mreijnde@users.noreply.github.com>
2023-01-21 11:33:53 +0100

    add JSON parse option to RS232 gateway

--- main/ZgatewayRS232.ino
+++ main/ZgatewayRS232.ino
@@ -46,7 +46,21 @@ void setupRS232() {
   Log.trace(F("ZgatewayRS232 setup done " CR));
 }
 
+
 void RS232toMQTT() {
+// Function to send retreived RS232 data as MQTT message
+#  if RS232toMQTTmode == 0
+  RS232RAWtoMQTT(); //Convert received data to single MQTT topic
+#  elif RS232toMQTTmode == 1
+  RS232JSONtoMQTT(); // Convert received data to multiple MQTT topics based on JSON (nested) keys
+#  else
+#    error("unsupported RS232toMQTTmode selected");
+#  endif
+}
+
+
+void RS232RAWtoMQTT() {
+  // Send all RS232 output until RS232InPost as MQTT message
   //This function is Blocking, but there should only ever be a few bytes, usually an ACK or a NACK.
   if (RS232Serial.available()) {
     Log.trace(F("RS232toMQTT" CR));
@@ -66,6 +80,70 @@ void RS232toMQTT() {
     Log.trace(F("Publish %s" CR), RS232data);
     char* output = RS232data + sizeof(RS232Pre) - 1;
     pub(subjectRS232toMQTT, output);
+  }
+}
+
+void RS232JSONtoMQTT() {
+  // Assumes valid JSON data at RS232 interface. Use (nested) keys to split JSON data in separate
+  // sub-MQTT-topics up to the defined nesting level.
+  if (RS232Serial->available()) {
+    Log.trace(F("RS232toMQTT" CR));
+
+    // Allocate the JSON document
+    StaticJsonDocument<RS232JSONDocSize> doc;
+
+    // Read the JSON document from the "link" serial port
+    DeserializationError err = deserializeJson(doc, *RS232Serial);
+
+    if (err == DeserializationError::Ok) {
+      // JSON received, send as MQTT topics
+      char topic[mqtt_topic_max_size + 1] = RS232toMQTTsubject;
+      sendMQTTfromNestedJson(doc.as<JsonVariant>(), topic, 0, RS232maxJSONlevel);
+    } else {
+      // Print error to serial log
+      Log.error(F("Error in RS232JSONtoMQTT, deserializeJson() returned %s"), err.c_str());
+
+      // Flush all bytes in the "link" serial port buffer
+      while (RS232Serial->available() > 0)
+        RS232Serial->read();
+    }
+  }
+}
+
+void sendMQTTfromNestedJson(JsonVariant obj, char* topic, int level, int maxLevel) {
+  // recursively step through JSON data and send MQTT messages
+  if (level < maxLevel && obj.is<JsonObject>()) {
+    int topicLength = strlen(topic);
+    // loop over fields
+    for (JsonPair pair : obj.as<JsonObject>()) {
+      // check if new key still fits in topic cstring
+      const char* key = pair.key().c_str();
+      Log.trace(F("level=%d, key='%s'" CR), level, pair.key().c_str());
+      if (topicLength + 2 + strlen(key) <= mqtt_topic_max_size) {
+        // add new level to existing topic cstring
+        topic[topicLength] = '/'; // add slash
+        topic[topicLength + 1] = '\0'; // terminate
+        strncat(topic + topicLength, key, mqtt_topic_max_size - topicLength - 2);
+
+        // step recursively into next level
+        sendMQTTfromNestedJson(pair.value(), topic, level + 1, maxLevel);
+
+        // restore topic
+        topic[topicLength] = '\0';
+      } else {
+        Log.error(F("Nested key '%s' at level %d does not fit within max topic length of %d, skipping"),
+                  key, level, mqtt_topic_max_size);
+      }
+    }
+
+  } else {
+    // output value at current json level
+    char output[256];
+    serializeJson(obj, output, 256);
+    Log.notice(F("level=%d, topic=%s, value: %s\n"), level, topic, output);
+
+    // send MQTT message
+    pub(topic, &output[0]);
   }
 }
 
--- main/config_RS232.h
+++ main/config_RS232.h
@@ -30,19 +30,51 @@ extern void setupRS232();
 extern void RS232toMQTT();
 extern void MQTTtoRS232(char* topicOri, JsonObject& RS232data);
 
-/*-------------------IR topics & parameters----------------------*/
-//RS232 MQTT Subjects
-#define subjectGTWRS232toMQTT     "/RS232toMQTT"
-#define subjectRS232toMQTT        "/RS232toMQTT"
-#define subjectMQTTtoRS232        "/commands/MQTTtoRS232"
-#define subjectForwardMQTTtoRS232 "home/gateway2/commands/MQTTtoRS232"
+/*-------------------RS232 topics & parameters----------------------*/
+
+// Settings RS232 to MQTT topic
+#define RS232toMQTTsubject "/RS232toMQTT"
+
+// setting to specify mode used for sending MQTT messages:
+//   0: RAW: all recieved input at RS232 interface is collected in single MQTT message
+//      defined by RS232toMQTTsubject
+//
+//   1: JSON: Assumes input at RS232 interface to be valid JSON. JSON keys are used to
+//      split the JSON data in separate MQTT sub-topics. This will be repeated for
+//      sub-keys up to the specified nesting level (RS232maxJSONlevel).
+//
+//      EXAMPLE: "{temperature: {sens1: 22, sens2: 23}, humidity: {sens1: 80, sens2: 60}}"
+//        - with RS232maxJSONlevel=1:
+//            ./RS232toMQTT/temperature  ==> "{sens1: 22, sens2: 23}"
+//            ./RS232toMQTT/humidity     ==> "{sens1: 80, sens2: 60}"
+//
+//        - with RS232maxJSONlevel=2 (or higher):
+//            ./RS232toMQTT/temperature/sens1 ==> 22
+//            ./RS232toMQTT/temperature/sens2 ==> 23
+//            ./RS232toMQTT/humidity/sens1 ==> 80
+//            ./RS232toMQTT/humidity/sens2 ==> 60
+//
+#ifndef RS232toMQTTmode
+#  define RS232toMQTTmode 1
+#endif
+
+// settings for RS232TopicMode 0 (RAW)
+#define RS232InPost '\n' // Hacky way to get last character of postfix for incoming
+#define MAX_INPUT   200 // how much serial data we expect
+
+// settings for RS232TopicMode 1 (JSON)
+#define RS232maxJSONlevel 2 // Max nested level in which JSON keys are converted to seperate sub-topics
+#define RS232JSONDocSize  1024 // bytes to reserve for the JSON doc
+
+// settings for MQTT to RS232
+#define subjectMQTTtoRS232 "/commands/MQTTtoRS232"
+#define RS232Pre           "00" // The prefix for the RS232 message
+#define RS232Post          "\r" // The postfix for the RS232 message
 
 //Setup for RS232
-#define MAX_INPUT   50 // how much serial data we expect
-#define RS232Baud   9600 // The serial connection Baud
-#define RS232Pre    "00" // The prefix for the RS232 message
-#define RS232Post   "\r" // The postfix for the RS232 message
-#define RS232InPost '\r' // Hacky way to get last character of postfix for incoming
+#ifndef RS232Baud
+#  define RS232Baud 9600 // The serial connection Baud
+#endif
 
 /*-------------------PIN DEFINITIONS----------------------*/
 #ifndef RS232_RX_GPIO
