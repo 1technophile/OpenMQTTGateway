aaa050d442de648a34575640a71d840ed6950f81
Northern Man <19808920+NorthernMan54@users.noreply.github.com>
2023-04-16 20:10:30 -0400

    Revert "Merge branch 'WebUIPR' of https://github.com/NorthernMan54/OpenMQTTGatewayProd into WebUIPR"
    This reverts commit 6c2776f0d21731b8390e178357c3b81cbd0434f6, reversing
    changes made to d5143325284005d4d87664f55c99b853791d79ae.

--- main/config_RF.h
+++ main/config_RF.h
@@ -81,7 +81,6 @@ struct RTL_433device {
   char uniqueId[uniqueIdSize];
   char modelName[modelNameSize];
   bool isDisc;
-  unsigned char count; // Counter for minimum number of messages before sending discover ( used a unsigned char, and stopped counting at 10)
 };
 
 const char parameters[40][4][24] = {
