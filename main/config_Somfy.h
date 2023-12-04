99082d450207190b2b97a1b35523f2360431fc02
Florian <1technophile@users.noreply.github.com>
2023-12-04 11:33:09 -0600

    [SOMFY] Remove unnecessary macro

--- main/config_Somfy.h
+++ main/config_Somfy.h
@@ -39,7 +39,4 @@ const uint32_t somfyRemotes[SOMFY_REMOTE_NUM] = {0x5184c8};
 /*-------------DEFINE YOUR MQTT PARAMETERS BELOW----------------*/
 #define subjectMQTTtoSomfy "/commands/MQTTtoSomfy"
 
-/*-------------------INTERNAL DEFINITIONS----------------------*/
-#define RF_FREQUENCY_SOMFY 433.42
-
 #endif
