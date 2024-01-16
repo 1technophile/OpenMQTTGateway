3d575b29836fd49290ae8021a35bd4791295c8bf
Florian <1technophile@users.noreply.github.com>
2024-01-16 09:31:05 -0600

    [DOCS] Add sitemap

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -107,4 +107,20 @@ module.exports = {
       }
   ]
   }
+  plugins: {
+    'sitemap': {
+      hostname: 'https://docs.openmqttgateway.com',
+      urls: [
+        'https://decoder.theengs.io/devices/devices.html',
+        'https://community.openmqttgateway.com/',
+        'https://shop.theengs.io/',
+        'https://shop.theengs.io/products/theengs-plug-smart-plug-ble-gateway-and-energy-consumption',
+        'https://shop.theengs.io/products/theengs-bridge-esp32-ble-mqtt-gateway-with-ethernet-and-external-antenna',
+        'https://app.theengs.io/',
+        'https://gateway.theengs.io/',
+        'https://decoder.theengs.io/',
+        'https://parser.theengs.io/',
+        'https://www.theengs.io/'
+      ],
+    },
 }
