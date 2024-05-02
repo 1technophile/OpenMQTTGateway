a2e4ccd28a1ec0a4cc5765b81724f89fa0128145
Vito Ammirata <vito.ammirata@gmail.com>
2024-05-02 21:34:57 +0200

    Update main.cpp

--- examples/LoraWaterCounter/src/main.cpp
+++ examples/LoraWaterCounter/src/main.cpp
@@ -15,44 +15,50 @@
 #define SS   18 // GPIO18 -- SX1278's CS
 #define RST  23 // GPIO23 -- SX1278's RESET
 #define DI0  26 // GPIO26 -- SX1278's IRQ(Interrupt Request)
-#define BAND 915E6
+#define BAND 433E6
 
-unsigned int counter = 0;
+//OLED pins
+#define OLED_SDA 21
+#define OLED_SCL 22 
+//#define OLED_RST 16
 
-SSD1306 display(0x3c, 4, 15);
+#define LED      25
+
+#define DHT_PIN  12
+#define DHT_TYPE DHT22 
+
+#define CNT_PIN  34
+
+#define BAT_PIN  35
+
+const int MAX_ANALOG_VAL = 4095;
+const float MAX_BATTERY_VOLTAGE = 4.2;
+
+int cntpkt = 0;
+unsigned long counter = 0;
+unsigned long last_interrupt_time = 0;
+int pulse_seen = 0;                                                   // pulse detected by interrupt function
+int bounce_delay_ms = 100;
+
+SSD1306 display(0x3c, OLED_SDA, OLED_SCL);
 String rssi = "RSSI --";
 String packSize = "--";
 String packet;
 
-float intTemperatureRead() {
-  SET_PERI_REG_BITS(SENS_SAR_MEAS_WAIT2_REG, SENS_FORCE_XPD_SAR, 3,
-                    SENS_FORCE_XPD_SAR_S);
-  SET_PERI_REG_BITS(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_CLK_DIV, 10,
-                    SENS_TSENS_CLK_DIV_S);
-  CLEAR_PERI_REG_MASK(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_POWER_UP);
-  CLEAR_PERI_REG_MASK(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_DUMP_OUT);
-  SET_PERI_REG_MASK(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_POWER_UP_FORCE);
-  SET_PERI_REG_MASK(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_POWER_UP);
-  ets_delay_us(100);
-  SET_PERI_REG_MASK(SENS_SAR_TSENS_CTRL_REG, SENS_TSENS_DUMP_OUT);
-  ets_delay_us(5);
-  float temp_f = (float)GET_PERI_REG_BITS2(SENS_SAR_SLAVE_ADDR3_REG,
-                                           SENS_TSENS_OUT, SENS_TSENS_OUT_S);
-  float temp_c = (temp_f - 32) / 1.8;
-  return temp_c;
-}
+DHT dht(DHT_PIN, DHT_TYPE); //Inizializza oggetto chiamato "dht", parametri: pin a cui è connesso il sensore, tipo di dht 11/22
+
+void ICACHE_RAM_ATTR bounceCheck ();
 
 void setup() {
-  pinMode(16, OUTPUT);
-  pinMode(2, OUTPUT);
+  pinMode(LED, OUTPUT);
 
-  digitalWrite(16, LOW); // set GPIO16 low to reset OLED
-  delay(50);
-  digitalWrite(16, HIGH); // while OLED is running, must set GPIO16 in high
+  //pinMode(OLED_RST, OUTPUT);
+  //digitalWrite(OLED_RST, LOW); // set GPIO16 low to reset OLED
+  //delay(50);
+  //digitalWrite(OLED_RST, HIGH); // while OLED is running, must set GPIO16 in high
 
   Serial.begin(115200);
-  while (!Serial)
-    ;
+  while (!Serial);
   Serial.println();
   Serial.println("LoRa Sender Test");
 
@@ -60,14 +66,18 @@ void setup() {
   LoRa.setPins(SS, RST, DI0);
   if (!LoRa.begin(BAND)) {
     Serial.println("Starting LoRa failed!");
-    while (1)
-      ;
+    while (1);
   }
 
   Serial.println("init ok");
   display.init();
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
+
+  pinMode(BAT_PIN, INPUT); 
+  pinMode(CNT_PIN, INPUT_PULLUP);
+  
+  attachInterrupt (CNT_PIN, bounceCheck, RISING);
 
   delay(1500);
 }
@@ -78,14 +88,18 @@ void loop() {
   display.setFont(ArialMT_Plain_10);
 
   display.drawString(0, 0, "Sending packet: ");
-  display.drawString(90, 0, String(counter));
+  display.drawString(90, 0, String(cntpkt));
 
   String NodeId = WiFi.macAddress();
-  float temp = intTemperatureRead();
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+  float battery = getBattery();
   // send packet
   LoRa.beginPacket();
   // Build json string to send
-  String msg = "{\"model\":\"ESP32TEMP\",\"id\":\"" + NodeId + "\",\"tempc\":" + String(temp) + "}";
+  //per test rimuovere
+
+  String msg = "{\"model\":\"ESP32CNT\",\"id\":\"" + NodeId + "\",\"count\":\"" + String(counter) + "\",\"tempc\":\"" + String(temp) + "\",\"hum\":\"" + String(hum) + "\",\"batt\":\"" + String(battery) + "\"}";
   // Send json string
   LoRa.print(msg);
   LoRa.endPacket();
@@ -93,23 +107,32 @@ void loop() {
   Serial.println(msg);
   
   display.drawString(0, 15, String(NodeId));
-  display.drawString(0, 30, "tempc: " + String(temp) + " C");
+  display.drawString(0, 30, "count: " + String(counter));
+  display.drawString(0, 45, "battery: " + String(battery)+ " %");
   display.display();
 
   delay(5000);
 
-  // Send makerfab soil sensor example packet
-  LoRa.beginPacket();
-  String msg2 = "ID010770 REPLY : SOIL INEDX:5862 H:100.00 T:1.77 ADC:624 BAT:857";
-  LoRa.print(msg2);
-  LoRa.endPacket();
+  cntpkt++;
 
-  Serial.println(msg2);
-
-  counter++;
-
-  digitalWrite(2, HIGH); // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(1000); // wait for a second
-  digitalWrite(2, LOW); // turn the LED off by making the voltage LOW
+  digitalWrite(LED, LOW); // turn the LED off by making the voltage LOW
   delay(60000); // wait for 60 seconds
 }
+
+void ICACHE_RAM_ATTR bounceCheck (){
+   unsigned long interrupt_time = millis();
+   if (interrupt_time - last_interrupt_time > bounce_delay_ms) counter++;    // void loop() then notes pulse == 1 and takes action      
+   last_interrupt_time = interrupt_time;
+}
+
+float getBattery(void) {
+  int rawValue = analogRead(BAT_PIN);
+
+  float voltageLevel = (rawValue / 4095.0) * 2 * 1.1 * 3.3; // calculate voltage level
+  float perc = voltageLevel / MAX_BATTERY_VOLTAGE * 100;
+
+  return(perc);
+}
+
