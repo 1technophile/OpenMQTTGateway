f6f11c1787b1279d84448e839c488505f7196336
Florian <1technophile@users.noreply.github.com>
2024-06-23 15:15:14 -0500

    Reduce procBLETask stack size

--- main/ZgatewayBT.ino
+++ main/ZgatewayBT.ino
@@ -902,7 +902,7 @@ void setupBTTasksAndBLE() {
       procBLETask, /* Function to implement the task */
       "procBLETask", /* Name of the task */
 #  ifdef USE_BLUFI
-      13500,
+      12000,
 #  else
       8500, /* Stack size in bytes */
 #  endif
